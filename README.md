bdd09fd2b47f69d75a37647c93b7314a89b9a496
Scott Henderson <3924836+scottyhq@users.noreply.github.com>
2025-07-22 16:47:55 -0700

    output centroids too

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # stac2geojson
 Because sometimes you just want one geojson
+
+## Create GeoJSON
+
+pixi run convert CATALOG="https://www.planet.com/data/stac/tanager-core-imagery/snow-ice/collection.json"
+python stac2geojson.py $CATALOG snow-ice.geojson --centroids
+
+We recommend adding the geojson outputs to a GitHub repository so that you can use different tools to visualize them:
+
+## Visualize GeoJSON
+
+### GitHub:
+- https://github.com/uw-cryo/stac2geojson/blob/main/planet/tanager/snow-ice.geojson
+
+### GeoJSON.io:
+- https://geojson.io/#id=github:uw-cryo/stac2geojson/blob/main/planet/tanager/snow-ice.geojson
+- https://geojson.io/#id=github:uw-cryo/stac2geojson/blob/main/planet/tanager/snow-ice-centroids.geojson
--- stac2geojson.py
+++ stac2geojson.py
@@ -2,6 +2,8 @@
 """
 Create a single GeoJSON FeatureCollection from a Static STAC Catalog/Collection URL
 
+Usage:
+python stac2geojson.py $CATALOG [output.geojson] [--centroids]
 
 Example:
 
@@ -9,16 +11,22 @@ Be sure to use the raw JSON URL, not the "browser" page:
 https://www.planet.com/data/stac/browser/tanager-core-imagery/snow-ice/collection.json
 
 CATALOG="https://www.planet.com/data/stac/tanager-core-imagery/snow-ice/collection.json"
-python stac2geojson.py $CATALOG snow-ice.geojson
+python stac2geojson.py $CATALOG snow-ice.geojson --centroids
 """
 
 import asyncio
 import rustac
 from rustac.store import HTTPStore
-import sys
+import geopandas as gpd
 import os
+import argparse
+import warnings
 
-async def catalog2geojson(catalog_url, output_path=None):
+# We'll just ignore this
+# UserWarning: 'crs' was not provided.  The output dataset will not have projection information defined and may not be usable in other systems.
+warnings.filterwarnings("ignore", message="'crs' was not provided.", category=UserWarning)
+
+async def catalog2geojson(catalog_url, output_path=None, output_centroids=False):
     """
     Given path to catalog.json read all child items & save as STAC-geoparquet
     """
@@ -33,11 +41,27 @@ async def catalog2geojson(catalog_url, output_path=None):
     if not output_path:
         output_path = "catalog.geojson"
 
+    # Also save polygon centroids for convenient visualization
+    # NOTE: requires geopandas and pyarrow dependencies
+    if output_centroids:
+        outfile = output_path.replace('.geojson', '-centroids.geojson')
+        table = rustac.to_arrow(items)
+        gf = gpd.GeoDataFrame.from_arrow(table)
+        cols = ["id","geometry","datetime"]
+        centroids = gf[cols].copy()
+        centroids['geometry'] = gf.centroid
+        centroids.to_file(outfile, driver='GeoJSON')
+
     # Many format options https://stac-utils.github.io/rustac-py/latest/api/#format
     await rustac.write(output_path, all_items, format="ndjson")
 
-
 if __name__ == "__main__":
-    catalog_url = sys.argv[1]
-    output_path = sys.argv[2] if len(sys.argv) > 2 else None
-    asyncio.run(catalog2geojson(catalog_url, output_path))
+    # TODO: automatically open browser? https://github.com/jwass/geojsonio.py
+
+    parser = argparse.ArgumentParser(description="Create a single GeoJSON FeatureCollection from a Static STAC Catalog/Collection URL")
+    parser.add_argument("catalog_url", help="URL to the STAC catalog or collection JSON")
+    parser.add_argument("output_path", nargs="?", default=None, help="Output GeoJSON file path")
+    parser.add_argument("--centroids", action="store_true", help="Output centroids instead of full geometries")
+    args = parser.parse_args()
+
+    asyncio.run(catalog2geojson(args.catalog_url, args.output_path, args.centroids))
